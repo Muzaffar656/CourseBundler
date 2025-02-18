1ad94af7d3b1428abc3af303a765758bb6ad0308
Muzaffar <106021084+Muzaffar656@users.noreply.github.com>
2025-02-18 14:08:53 +0530

    serverl url change

--- CourseBundler_Frontend1X/src/Redux/store.js
+++ CourseBundler_Frontend1X/src/Redux/store.js
@@ -19,4 +19,4 @@ const store = configureStore({
 
 export default store
 
-export const server =  process.env.REACT_APP_URL || 'http://localhost:7000/api/v1'
+export const server =  `${process.env.REACT_APP_URL}/api/v1` || 'http://localhost:7000/api/v1'
